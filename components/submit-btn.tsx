b6553951cc1860ca2fbb39535fd1c5f774d28b20
Miltos <dai17079@uom.edu.gr>
2025-10-16 18:10:39 +0300

    updates

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,6 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function SubmitBtn() {
+  const [isPending, setIsPending] = useState(false);
+
+  return (
+    <button
+      type="submit"
+      disabled={isPending}
+      className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:bg-opacity-65 dark:bg-white dark:bg-opacity-10"
+    >
+      {isPending ? (
+        <>
+          <div className="h-5 w-5 animate-spin rounded-full border-2 border-white border-t-transparent"></div>
+          Sending...
+        </>
+      ) : (
+        <>
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
+        </>
+      )}
+    </button>
+  );
+}
+
+{
+  /*
+
+  "use client";
+
 import React from "react";
 import { FaPaperPlane } from "react-icons/fa";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
   const { pending } = useFormStatus();
@@ -21,4 +55,6 @@ export default function SubmitBtn() {
       )}
     </button>
   );
+}
+*/
 }
