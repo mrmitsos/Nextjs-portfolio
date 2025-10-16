531b63ba28530fa20f13c989437207f6dfbff413
Miltos <dai17079@uom.edu.gr>
2025-10-16 16:49:22 +0300

    removed some files

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,6 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
   {
@@ -30,35 +27,5 @@ export const links = [
   {
     name: "Contact",
     hash: "#contact",
-  },
-] as const;
-
-export const projectsData = [
-  {
-    title: "CorpComment",
-    slug: "corpcomment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-    projectUrl: "https://corpcomment.com"
-  },
-  {
-    title: "rmtDev",
-    slug: "rmtdev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-    projectUrl: "https://rmtdev.com"
-  },
-  {
-    title: "Word Analytics",
-    slug: "wordanalytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-    projectUrl: "https://wordanalytics.app"
   },
 ] as const;
