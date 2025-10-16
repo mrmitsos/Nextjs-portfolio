0f1f226f50c428e84a739d733221d8d21b8909a4
Miltos <dai17079@uom.edu.gr>
2025-10-16 20:32:45 +0300

    updated send email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -29,7 +29,7 @@ export const sendEmail = async (formData: FormData) => {
       from: "Contact Form <onboarding@resend.dev>",
       to: "dimitriosmaleas@hotmail.com",
       subject: "Hello World",
-      reply_to: senderEmail as string,
+      replyTo: senderEmail as string,
       react: React.createElement(ContactFormEmail, {
         message: message as string,
         senderEmail: senderEmail as string,
